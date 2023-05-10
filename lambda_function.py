1a925d1e320a15fb44b6ca97a975a8306f4a1d26
Raven Spencer <107158921+RavenEsc@users.noreply.github.com>
2023-05-09 19:40:26 -0500

    Stage 2.3

--- lambda_function.py
+++ lambda_function.py
@@ -1,28 +1,30 @@
-# import the json utility package since we will be working with a JSON object
+# Time = *, Name = **
+
+# ESSENTIAL (resources)
 import json
-# import the AWS SDK (for Python the package name is boto3)
 import boto3
-# import two packages to help us with dates and date formatting
+# import two packages to help us with dates and date formatting *
 from time import gmtime, strftime
 
-# create a DynamoDB object using the AWS SDK
+# ESSENTIAL (Link to database)
 dynamodb = boto3.resource('dynamodb')
-# use the DynamoDB object to select our table
 table = dynamodb.Table('helloWorldVisitorCounter')
-# store the current time in a human readable format in a variable
+# store the current time in a human readable format in a variable *
 now = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
 
-# define the handler function that the Lambda service will use as an entry point
+# ESSENTIAL (link to serverless machine)
 def lambda_handler(event, context):
-# extract values from the event object we got from the Lambda service and store in a variable
+# extract values from the event object we got from the Lambda service and store in a variable **
     name = event['firstName'] +' '+ event['lastName']
-# write name and time to the DynamoDB table using the object we instantiated and save response in a variable
+
+# write name and time to the DynamoDB table using the object we instantiated and save response in a variable * **
     response = table.put_item(
         Item={
             'ID': name,
             'LatestGreetingTime':now
             })
-# return a properly formatted JSON object
+    
+# return a properly formatted JSON object (Output)
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda, ' + name)
