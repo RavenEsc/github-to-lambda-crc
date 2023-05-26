173a200dc3d3ce17955f993fd59609cec113d5db
Raven Spencer <107158921+RavenEsc@users.noreply.github.com>
2023-05-26 15:33:54 -0500

    Commented out the 2nd lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -11,7 +11,7 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('helloWorldVisitorCounter')
 
 # Func for counting the amount of times this code is executed in lambda (oc=overall count)
-def lambda_handler_oc(event, context):
+def lambda_handler(event, context):
     response = table.update_item(
         Key={
             'ID': 'visit-count'
@@ -34,16 +34,16 @@ def lambda_handler_oc(event, context):
         'body': 'Count: ' + str(count)
     }
 
-# Func for showing when the site was visited in lambda by number (toa=time of arrival), then sends a json dump back to the site
-def lambda_handler_toa(event, context):
-    response = table.put_item(
-        Item={
-            'count': str(count),
-            'VisitedTime': now
-            }
-    )
+# # Func for showing when the site was visited in lambda by number (toa=time of arrival), then sends a json dump back to the site
+# def lambda_handler(event, context):
+#     response = table.put_item(
+#         Item={
+#             'count': str(count),
+#             'VisitedTime': now
+#             }
+#     )
     
-    return {
-        'statusCode': 200,
-        'body': json.dumps("Hello from Lambda, No." + str(count) + ", at " + str(now) + ".")
-    }
+#     return {
+#         'statusCode': 200,
+#         'body': json.dumps("Hello from Lambda, No." + str(count) + ", at " + str(now) + ".")
+#     }
